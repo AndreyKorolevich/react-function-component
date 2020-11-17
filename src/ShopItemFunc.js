bb3bdb54074388270a79db69664f95e9e373460c
andreykorolevich <korolevich1994@mail.ru>
2020-11-18 01:05:25 +0300

    Function component

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+function ShopItemFunc({item}) {
+    return (
+        <div className="main-content">
+            <h2>{item.brand}</h2>
+            <h1>{item.title}</h1>
+            <h3>{item.description}</h3>
+            <div className="description">
+                {item.descriptionFull}
+            </div>
+            <div className="highlight-window mobile">
+                <div className="highlight-overlay"></div>
+            </div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{item.price + item.currency}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
+}
+
+export default ShopItemFunc;
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.shape({
+        brand: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        descriptionFull: PropTypes.string,
+        price: PropTypes.number.isRequired,
+        currency: PropTypes.string
+    })
+}
